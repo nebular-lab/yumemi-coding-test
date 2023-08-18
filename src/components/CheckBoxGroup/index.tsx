3b302965f24cc01b593dea17a27ad3e16a1fb4a8
nebular-lab <hiranodaisuke0721@gmail.com>
2023-08-19 06:59:03 +0900

    radioButtonとcheckboxをそれぞれ別のコンポーネントに切り分ける

--- src/components/CheckBoxGroup/index.tsx
+++ src/components/CheckBoxGroup/index.tsx
@@ -1,29 +1,45 @@
 import { FC } from 'react'
 import { Prefecture } from '../../types'
 
-type Props = {
+type CheckBoxGroupProps = {
   prefectures: Prefecture[]
   checkedPrefCodes: number[]
   toggleCheckedPrefCodes: (prefCode: number) => void
 }
-
-const CheckBoxGroup: FC<Props> = (props) => {
+const CheckBoxGroup: FC<CheckBoxGroupProps> = (props) => {
   const { prefectures, checkedPrefCodes, toggleCheckedPrefCodes } = props
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
       {prefectures.map((prefecture) => (
-        <div key={prefecture.prefCode}>
-          <input
-            id={prefecture.prefName}
-            type='checkbox'
-            checked={checkedPrefCodes.includes(prefecture.prefCode)}
-            onChange={() => toggleCheckedPrefCodes(prefecture.prefCode)}
-          />
-          <label htmlFor={prefecture.prefName}>{prefecture.prefName}</label>
-        </div>
+        <CheckBox
+          prefecture={prefecture}
+          checkedPrefCodes={checkedPrefCodes}
+          toggleCheckedPrefCodes={toggleCheckedPrefCodes}
+        />
       ))}
     </div>
   )
 }
+
+type CheckBoxProps = {
+  prefecture: Prefecture
+  checkedPrefCodes: number[]
+  toggleCheckedPrefCodes: (prefCode: number) => void
+}
+const CheckBox = (props: CheckBoxProps) => {
+  const { prefecture, checkedPrefCodes, toggleCheckedPrefCodes } = props
+  return (
+    <div key={prefecture.prefCode}>
+      <input
+        id={prefecture.prefName}
+        type='checkbox'
+        checked={checkedPrefCodes.includes(prefecture.prefCode)}
+        onChange={() => toggleCheckedPrefCodes(prefecture.prefCode)}
+      />
+      <label htmlFor={prefecture.prefName}>{prefecture.prefName}</label>
+    </div>
+  )
+}
+
 export default CheckBoxGroup
