57011c2fe43b2d0cf4607c08574d293ce0f94ebe
nebular-lab <hiranodaisuke0721@gmail.com>
2023-08-19 05:39:48 +0900

    getPrefecturePopulationをリファクタ

--- src/api/getPrefecturePopulation.ts
+++ src/api/getPrefecturePopulation.ts
@@ -2,39 +2,43 @@ import { useQuery } from '@tanstack/react-query'
 import { axios } from '../lib/axios'
 import { PopulationDataSchema, PrefectureArraySchema } from '../types'
 
+const fetchPopulationDataForPrefecture = async (prefCode: number) => {
+  const { data: populationResponseData } = await axios.get(
+    '/population/composition/perYear',
+    {
+      params: {
+        prefCode,
+        cityCode: '-',
+      },
+    },
+  )
+  return PopulationDataSchema.parse(populationResponseData.result.data)
+}
+
+const fetchPrefectures = async () => {
+  const { data: prefectureResponseData } = await axios.get('/prefectures')
+  return PrefectureArraySchema.parse(prefectureResponseData.result)
+}
+
 export const useQueryPrefecturePopulation = () => {
   const fetchPrefecturePopulation = async () => {
-    const { data: prefectureResponseData } = await axios.get('/prefectures')
-    const prefectures = PrefectureArraySchema.parse(
-      prefectureResponseData.result,
-    )
+    const prefectures = await fetchPrefectures()
 
     const populationData = await Promise.all(
-      prefectures.map(async (prefecture) => {
-        const { data: populationResponseData } = await axios.get(
-          '/population/composition/perYear',
-          {
-            params: {
-              prefCode: prefecture.prefCode,
-              cityCode: '-',
-            },
-          },
-        )
-        return PopulationDataSchema.parse(populationResponseData.result.data)
-      }),
+      prefectures.map(({ prefCode }) =>
+        fetchPopulationDataForPrefecture(prefCode),
+      ),
     )
 
-    // 人口タイプのラベル ['総人口', '年少人口', '生産年齢人口', '老年人口']
-    const populationTypeLabels = populationData[0].map((LabeledPopulation) => {
-      return LabeledPopulation.label
-    })
-    // 年 [1980, 1985, 1990, ...]
-    const years = populationData[0][0].data.map((population) => {
-      return population.year
-    })
+    // Assuming populationData[0] exists
+    const firstDataSet = populationData[0]
+
+    const populationTypeLabels = firstDataSet.map(({ label }) => label)
+    const years = firstDataSet[0].data.map(({ year }) => year)
 
     return { prefectures, populationTypeLabels, years, populationData }
   }
+
   return useQuery({
     queryKey: ['prefecturePopulation'],
     queryFn: fetchPrefecturePopulation,
