29ca48df67980310a262ff26013254398a86fb44
nebular-lab <hiranodaisuke0721@gmail.com>
2023-08-19 05:32:18 +0900

    エラーバウンダリの修正

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,20 @@ import ChartPage from './ChartPage'
 import { AppProvider } from './providers/app'
 import { ErrorBoundary } from 'react-error-boundary'
 import Spinner from './components/Spiner'
+
+const ErrorFallback = ({ error }: { error: Error }) => (
+  <div>
+    <h2>エラーが発生しました</h2>
+    <p>{error.message}</p>
+  </div>
+)
+
 const App = () => {
   return (
     <AppProvider>
       <h1>ゆめみ コーディングテスト</h1>
       <ErrorBoundary
-        fallback={<div>データの読み取り最中にエラーが起きました</div>}
+        fallbackRender={({ error }) => <ErrorFallback error={error} />}
       >
         <Suspense fallback={<Spinner />}>
           <ChartPage />
